19f1cfd98a801bd1432fb257691c439643331501
Foxroad1 <145580419+Foxroad1@users.noreply.github.com>
2024-12-15 21:05:43 +0200

    app.py messing up

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from functools import wraps
 import jwt
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session, send_file
 import mariadb
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -16,7 +17,7 @@ def get_db_connection():
         conn = mariadb.connect(
             user="root",
             password="1542",
-            host="10.6.128.19",  # Use the school ip
+            host="10.6.128.19",  # Use the school IP
             port=3306,
             database="bethon_worker"
         )
@@ -47,11 +48,9 @@ def test_db():
     except Exception as e:
         return f'Error: {e}'
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -75,15 +74,13 @@ def register():
         cur.execute(
             "INSERT INTO employees (id, name, dob, title, start_date, worker_id, username, password, email, social_security_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             (new_id, data['name'], data['dob'], data['title'], data['start_date'], worker_id, data['username'],
-             hashed_password,
-             data['email'], data['social_security_number'])
+             hashed_password, data['email'], data['social_security_number'])
         )
         conn.commit()
         cur.close()
         conn.close()
         return redirect(url_for('web_login'))
     return render_template('register.html')
-
 
 @app.route('/web_login', methods=['GET', 'POST'])
 def web_login():
@@ -106,7 +103,7 @@ def web_login():
                 # Log the login time
                 cur.execute(
                     "INSERT INTO work_logs (employee_id, log_in_time) VALUES (?, ?)",
-                    (session['user_id'], datetime.now())
+                    (session['user_id'], datetime.now(timezone.utc))
                 )
                 conn.commit()
             cur.close()
@@ -114,7 +111,6 @@ def web_login():
             return redirect(url_for('profile'))
         return 'Invalid username or password'
     return render_template('web_login.html')
-
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -124,14 +120,13 @@ def logout():
         cur = conn.cursor()
         cur.execute(
             "UPDATE work_logs SET log_out_time = ? WHERE employee_id = ? AND log_out_time IS NULL",
-            (datetime.now(), session['user_id'])
+            (datetime.now(timezone.utc), session['user_id'])
         )
         conn.commit()
         cur.close()
         conn.close()
         session.pop('user_id', None)
     return redirect(url_for('web_login'))
-
 
 @app.route('/profile')
 def profile():
@@ -152,8 +147,8 @@ def profile():
     monthly_summary = {}
 
     for log in work_logs:
-        log_in_time = log[2]
-        log_out_time = log[3] if log[3] else datetime.now()  # Use current time if still logged in
+        log_in_time = log[2].replace(tzinfo=timezone.utc)
+        log_out_time = log[3].replace(tzinfo=timezone.utc) if log[3] else datetime.now(timezone.utc)
         date_str = log_in_time.strftime('%Y-%m-%d')
 
         # Calculate the duration of each work session
@@ -183,7 +178,6 @@ def profile():
                            daily_summary=daily_summary,
                            weekly_summary=weekly_summary,
                            monthly_summary=monthly_summary)
-
 
 @app.route('/log_work', methods=['GET', 'POST'])
 def log_work():
@@ -232,7 +226,7 @@ def api_login():
     cur.close()
     conn.close()
     if user and check_password_hash(user[7], data['password']):
-        token = jwt.encode({'user_id': user[0], 'exp': datetime.utcnow() + timedelta(hours=24)}, app.secret_key)
+        token = jwt.encode({'user_id': user[0], 'exp': datetime.now(timezone.utc) + timedelta(hours=24)}, app.secret_key)
         return jsonify({'token': token})
     return jsonify({'message': 'Invalid username or password'}), 401
 
@@ -265,15 +259,33 @@ def log_start_time(current_user):
     data = request.get_json()
     qr_code_data = data.get('qrCode')
 
+
     # No need to validate QR code again, assume Android app has validated it
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO work_logs (employee_id, log_in_time, qr_code) VALUES (?, ?, ?)", (current_user, datetime.utcnow(), qr_code_data))
+    cur.execute("INSERT INTO work_logs (employee_id, log_in_time, qr_code) VALUES (?, ?, ?)", (current_user, datetime.now(timezone.utc), qr_code_data))
     conn.commit()
     cur.close()
     conn.close()
     return jsonify({'message': 'Start time logged successfully'}), 200
+
+
+@app.route('/api/logout', methods=['POST'])
+@token_required
+def api_logout(current_user):
+    # Log the logout time
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "UPDATE work_logs SET log_out_time = ? WHERE employee_id = ? AND log_out_time IS NULL",
+        (datetime.now(timezone.utc), current_user)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({'message': 'Logout successful'}), 200
 
 @app.route('/api/log_end_time', methods=['POST'])
 @token_required
@@ -286,7 +298,7 @@ def log_end_time(current_user):
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("UPDATE work_logs SET log_out_time = ? WHERE employee_id = ? AND log_out_time IS NULL", (datetime.utcnow(), current_user))
+    cur.execute("UPDATE work_logs SET log_out_time = ? WHERE employee_id = ? AND log_out_time IS NULL", (datetime.now(timezone.utc), current_user))
     conn.commit()
     cur.close()
     conn.close()
@@ -361,7 +373,6 @@ def admin():
 
     return render_template('admin.html', employees=employees, work_logs=work_logs)
 
-
 @app.route('/admin_login', methods=['GET', 'POST'])
 def admin_login():
     if request.method == 'POST':
@@ -374,14 +385,11 @@ def admin_login():
         return 'Invalid username or password'
     return render_template('admin_login.html')
 
-
 @app.route('/admin_logout', methods=['POST'])
 def admin_logout():
     session.pop('admin_logged_in', None)
     return redirect(url_for('admin_login'))
 
-
-# Function to save work logs to a CSV file with calculated work hours in hours, minutes, and seconds format
 def save_work_logs_to_csv(logs, filename):
     with open(filename, 'w', newline='') as csvfile:
         fieldnames = ['Employee ID', 'Log In Time', 'Log Out Time', 'Title', 'Work Hours']
@@ -389,12 +397,12 @@ def save_work_logs_to_csv(logs, filename):
 
         writer.writeheader()
         for log in logs:
-            log_in_time = log[2]
-            log_out_time = log[3] if log[3] else datetime.now()  # Use current time if still logged in
+            log_in_time = log[2].replace(tzinfo=timezone.utc)
+            log_out_time = log[3].replace(tzinfo=timezone.utc) if log[3] else datetime.now(timezone.utc)
             duration = log_out_time - log_in_time
             hours, remainder = divmod(duration.total_seconds(), 3600)
             minutes, seconds = divmod(remainder, 60)
-            work_hours = f'{int(hours)}h {int(minutes)}m {int(seconds)}s'  # Convert duration to HH:MM:SS
+            work_hours = f'{int(hours)}h {int(minutes)}m {int(seconds)}s'
             writer.writerow({
                 'Employee ID': log[1],
                 'Log In Time': log_in_time,
@@ -403,22 +411,20 @@ def save_work_logs_to_csv(logs, filename):
                 'Work Hours': work_hours
             })
 
-
-# Function to generate and save work logs for a specific period
 def generate_work_logs(period):
     conn = get_db_connection()
     cur = conn.cursor()
 
     if period == 'daily':
-        date_str = datetime.now().strftime('%Y-%m-%d')
+        date_str = datetime.now(timezone.utc).strftime('%Y-%m-%d')
         cur.execute("SELECT * FROM work_logs WHERE DATE(log_in_time) = %s", (date_str,))
         filename = f'work_logs/work_logs_daily_{date_str}.csv'
     elif period == 'weekly':
-        week_str = datetime.now().strftime('%Y-%U')
-        cur.execute("SELECT * FROM work_logs WHERE YEARWEEK(log_in_time, 1) = YEARWEEK(%s, 1)", (datetime.now(),))
+        week_str = datetime.now(timezone.utc).strftime('%Y-%U')
+        cur.execute("SELECT * FROM work_logs WHERE YEARWEEK(log_in_time, 1) = YEARWEEK(%s, 1)", (datetime.now(timezone.utc),))
         filename = f'work_logs/work_logs_weekly_{week_str}.csv'
     elif period == 'monthly':
-        month_str = datetime.now().strftime('%Y-%m')
+        month_str = datetime.now(timezone.utc).strftime('%Y-%m')
         cur.execute("SELECT * FROM work_logs WHERE DATE_FORMAT(log_in_time, '%Y-%m') = %s", (month_str,))
         filename = f'work_logs/work_logs_monthly_{month_str}.csv'
 
@@ -429,23 +435,21 @@ def generate_work_logs(period):
     save_work_logs_to_csv(logs, filename)
     return filename
 
-
-# Function to generate and save work logs for a specific employee and period
 def generate_employee_work_logs(employee_id, period):
     conn = get_db_connection()
     cur = conn.cursor()
 
     if period == 'daily':
-        date_str = datetime.now().strftime('%Y-%m-%d')
+        date_str = datetime.now(timezone.utc).strftime('%Y-%m-%d')
         cur.execute("SELECT * FROM work_logs WHERE employee_id = ? AND DATE(log_in_time) = ?", (employee_id, date_str))
         filename = f'work_logs/work_logs_daily_{employee_id}_{date_str}.csv'
     elif period == 'weekly':
-        week_str = datetime.now().strftime('%Y-%U')
+        week_str = datetime.now(timezone.utc).strftime('%Y-%U')
         cur.execute("SELECT * FROM work_logs WHERE employee_id = ? AND YEARWEEK(log_in_time, 1) = YEARWEEK(?, 1)",
-                    (employee_id, datetime.now()))
+                    (employee_id, datetime.now(timezone.utc)))
         filename = f'work_logs/work_logs_weekly_{employee_id}_{week_str}.csv'
     elif period == 'monthly':
-        month_str = datetime.now().strftime('%Y-%m')
+        month_str = datetime.now(timezone.utc).strftime('%Y-%m')
         cur.execute("SELECT * FROM work_logs WHERE employee_id = ? AND DATE_FORMAT(log_in_time, '%Y-%m') = ?",
                     (employee_id, month_str))
         filename = f'work_logs/work_logs_monthly_{employee_id}_{month_str}.csv'
@@ -457,7 +461,6 @@ def generate_employee_work_logs(employee_id, period):
     save_work_logs_to_csv(logs, filename)
     return filename
 
-
 # Route to download work logs for a specific employee and period
 @app.route('/download_employee_work_logs/<employee_id>/<period>')
 def download_employee_work_logs(employee_id, period):
@@ -468,11 +471,9 @@ def download_employee_work_logs(employee_id, period):
 
     return send_file(filename, as_attachment=True)
 
-
 # Ensure the directory for storing CSV files exists
 if not os.path.exists('work_logs'):
     os.makedirs('work_logs')
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
